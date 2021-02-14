ef7364b6fedcbb472824cef607f44b82d634021b
Didrick <didrick.lecornet@gmail.com>
2021-02-14 18:26:57 +0100

    fixes

--- classes/Entity.py
+++ classes/Entity.py
@@ -6,26 +6,23 @@ class Entity():
         self.vitesse = 100
         self.attaque = 1
         self.defence = 0
-        #self.x = x
-        #self.y = y
-        #self.hitbox = pygame.Rect(self.x, self.y, 50, 100)
         img = pygame.image.load('images/red.png')
         self.image = pygame.transform.scale(img, (40,80))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.hitbox = self.image.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
         pass
 
     def move(self):
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
-            self.rect.x -= 7
-            if self.rect.x < 0:
-                self.rect.x = 0
+            self.hitbox.x -= 8
+            if self.hitbox.x < 0:
+                self.hitbox.x = 0
         if key[pygame.K_RIGHT]:
-            self.rect.x += 7
-            if self.rect.x > 430: # a auto
-                self.rect.x = 430
+            self.hitbox.x += 8
+            if self.hitbox.x > 480: # a auto
+                self.hitbox.x = 480
         #self.hitbox = pygame.Rect(self.x, self.y, 50, 100)
         #y = y-taille de case * vitesse
 
